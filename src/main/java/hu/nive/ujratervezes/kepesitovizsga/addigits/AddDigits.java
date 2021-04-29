8d8bd72be6146c89c1b92fa19cc348dcf48419f7
BeneZ <bezraat@gmail.com>
2021-04-29 10:16:35 +0200

    Commit AddDigits after form

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -3,12 +3,12 @@ package hu.nive.ujratervezes.kepesitovizsga.addigits;
 public class AddDigits {
 
 
-    public int addDigits(String input){
-        if (input==null || input.isBlank()) {
+    public int addDigits(String input) {
+        if (input == null || input.isBlank()) {
             return -1;
         }
         return input.chars()
-                .filter(c -> c>='0' && c<='9' )
+                .filter(c -> c >= '0' && c <= '9')
                 .map(Character::getNumericValue)
                 .sum();
     }
